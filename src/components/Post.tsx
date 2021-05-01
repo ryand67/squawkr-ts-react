ec00517407cd5048d9c3588c1215d56cfa5f4b22
ryand67 <rwdevenney@gmail.com>
2021-04-30 21:22:57 -0400

    delete posts functional

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import { convertTimestamp } from 'convert-firebase-timestamp';
 import { Link } from 'react-router-dom';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../util/firebase';
+
 export type fbDate = {
     seconds: number;
     nanoseconds: number;
@@ -16,12 +19,23 @@ interface Props {
 }
 
 function Post({ content, id, date, author, email }: Props) {
+
+    const [user] = useAuthState(auth);
+    
+    //Checks if person who is logged in is the person who wrote the post.
+    let isPostOwner: Boolean = user?.email === email;
     
     let newDate = convertTimestamp(date).toString().substr(0, 24);
 
+    const handleDelete = (id: string): void => {
+        db.collection('posts').doc(id).delete().then(() => {
+            window.location.reload();
+        })
+    }
+
     return (
         <PostCard>
-            <DeleteButton></DeleteButton>
+            {isPostOwner ? <DeleteButton onClick={() => handleDelete(id)}>X</DeleteButton> : ''}
             <Link to={`:username=${email}`}><Author>@{author}</Author></Link>
             <Content>{content}</Content>
             <Date>{newDate}</Date>
@@ -30,6 +44,7 @@ function Post({ content, id, date, author, email }: Props) {
 }
 
 const PostCard = styled.div`
+    position: relative;
     width: 100%;
     background: blue;
     height: auto;
@@ -52,7 +67,9 @@ const Content = styled.p``;
 const Date = styled.p``;
 
 const DeleteButton = styled.p`
-
+    position: absolute;
+    top: 0;
+    right: 0;
 `;
 
 export default Post
